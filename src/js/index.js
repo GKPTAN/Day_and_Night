576ff6d087002e489cf636e35e650a9eca1c68dd
GKPTAN <146596464+GKPTAN@users.noreply.github.com>
2024-11-14 21:20:17 -0300

    projeto finalizado
    adição das estrelas usando o arquivo particle.js durante a noite

--- src/js/index.js
+++ src/js/index.js
@@ -2,16 +2,26 @@ let toggleWrapper = document.getElementById("dn");
 const noite = document.getElementById('noite');
 const lua = document.getElementById('lua');
 const sol = document.getElementById('clima');
+const contribuidor = document.getElementById('ajudante');
+const estrelas = document.getElementById('particles-js');
 toggleWrapper.addEventListener("click", () => {
+    toggleWrapper.disabled = true;
+    setTimeout(() => {
+        toggleWrapper.disabled = false;
+    }, 4000);
     noite.classList.toggle('noite');
     lua.classList.toggle('lua');
     lua.classList.toggle('lua-hidden');
     if (sol.style.left === '150%') {
+        estrelas.style.opacity = '0';
+        contribuidor.style.color = 'black';
         sol.classList.remove('clima');
         sol.style.left = '70%';
         void sol.offsetWidth;
         sol.classList.add('clima');
     } else {
+        estrelas.style.opacity = '1';
         sol.style.left = '150%';
+        contribuidor.style.color = 'white';
     };
 });
